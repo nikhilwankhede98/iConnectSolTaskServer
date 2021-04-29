2489f98b37deca70af492b6b9e905c4c22e6fe1c
Nikhil Wankhede <nikhil@eilaconsulting.com>
2021-04-30 02:20:41 +0530

    version 1.0

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const config = require('config')
+const cors = require ('cors')
 
 
 const app = express()
 
 //Body Parser Middleware
 app.use(express.json());
-
+app.use(cors());
 //DB Config
 const db = config.get('mongoURI')
 
@@ -24,16 +25,6 @@ mongoose
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-
-//Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//     //Set Static Folder
-//     app.use(express.static('client/build'));
-  
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
 
 const PORT = process.env.PORT || 5000
 
